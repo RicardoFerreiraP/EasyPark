4f1599da60b89bb6ede1e8c054ed273d6ec9a505
carlos alexandre Neppel <carlos_neppel@hotmail.com>
2018-09-09 14:40:44 -0300

    Automovel Controller

--- EasyPark/EasyPark/DAL/AutomovelDAO.cs
+++ EasyPark/EasyPark/DAL/AutomovelDAO.cs
@@ -18,5 +18,33 @@ namespace EasyPark.DAL
         {
             return ctx.Automovel.FirstOrDefault(x => x.Placa.Equals(automovel.Placa));
         }
+
+        public static bool CadastrarAutomovel(Automovel automovel)
+        {
+            if (BuscaAutomovelPorPlaca(automovel) == null)
+            {
+                ctx.Automovel.Add(automovel);
+                ctx.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public static List<Automovel> ListarTodosAutomoveis()
+        {
+            return ctx.Automovel.ToList();
+        }
+
+        public static void AlterarAutomovel(Automovel automovel)
+        {
+            ctx.Entry(automovel).State = EntityState.Modified;
+            ctx.SaveChanges();
+        }
+
+        public static void RemoverAutomovel(int id )
+        {
+            ctx.Automovel.Remove(BuscaAutomovelPorId(id));
+            ctx.SaveChanges();
+        }
     }
 }
