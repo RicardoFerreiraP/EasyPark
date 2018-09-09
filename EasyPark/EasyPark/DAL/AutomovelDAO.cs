aeae772a2233536477e260c609060a314f3fe25e
carlos alexandre Neppel <carlos_neppel@hotmail.com>
2018-09-09 13:50:39 -0300

    AutomovelDAO

--- EasyPark/EasyPark/DAL/AutomovelDAO.cs
+++ EasyPark/EasyPark/DAL/AutomovelDAO.cs
@@ -1,6 +1,22 @@
-﻿namespace EasyPark.DAL
+﻿using System;
+using System.Collections.Generic;
+using EasyPark.Models;
+using System.Linq;
+using System.Data.Entity;
+
+namespace EasyPark.DAL
 {
     public class AutomovelDAO
     {
+        private static Context ctx = SingletonContext.GetInstance();
+        public static Automovel BuscaAutomovelPorId(int id)
+        {
+            return ctx.Automovel.Find(id);
+        }
+
+        public static Automovel BuscaAutomovelPorPlaca(Automovel automovel)
+        {
+            return ctx.Automovel.FirstOrDefault(x => x.Placa.Equals(automovel.Placa));
+        }
     }
 }
