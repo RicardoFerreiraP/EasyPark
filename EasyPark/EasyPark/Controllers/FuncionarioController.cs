a26251e93c1e27b4bd27056a2fe2f38b980c88ce
Hellen <hellencarolinecbj@hotmail.com>
2018-09-14 09:03:18 -0300

    CRUD Funcionario completo

--- EasyPark/EasyPark/Controllers/FuncionarioController.cs
+++ EasyPark/EasyPark/Controllers/FuncionarioController.cs
@@ -14,10 +14,21 @@ namespace EasyPark.Controllers
         {
             return View();
         }
+
+       public ActionResult FuncionariosCadastrados()
+        {
+            return View(FuncionarioDAO.ListarTodosFuncionarios());
+        }
+
+        public ActionResult Teste()
+        {
+            return View();
+        }
         public ActionResult CadastrarFuncionario()
         {
             return View();
         }
+
         [HttpPost]
         public ActionResult CadastrarFuncionario([Bind(Include ="FuncionarioID, Nome, CPF, Telefone, Senha, ConfirmacaoDaSenha")] Funcionario  funcionario, HttpPostedFileBase fupImagem)
         {
@@ -39,7 +50,7 @@ namespace EasyPark.Controllers
                 if(FuncionarioDAO.CadastrarFuncionario(funcionario))
                 {
                     FuncionarioDAO.CadastrarFuncionario(funcionario);
-                    return RedirectToAction("ClientesCadastrados", "Cliente");
+                    return RedirectToAction("FuncionariosCadastrados", "Funcionario");
                 }
                 ModelState.AddModelError("", "Já existe um funcionario com este CPF!!");
                 return View(funcionario);
@@ -49,10 +60,40 @@ namespace EasyPark.Controllers
                 return View(funcionario);
             }
         }
+
+        public ActionResult AlterarFuncionario(int id)
+        {
+            return View(FuncionarioDAO.BuscarFuncionarioPorID(id));
+        }
+
+        [HttpPost]
+        public ActionResult AlterarFuncionario(Funcionario funcionarioAlterado)
+        {
+           Funcionario funcionarioOriginal = FuncionarioDAO.BuscarFuncionarioPorID(funcionarioAlterado.FuncionarioID);
+
+            funcionarioOriginal.Nome     = funcionarioAlterado.Nome;
+            funcionarioOriginal.CPF      = funcionarioAlterado.CPF;
+            funcionarioOriginal.Telefone = funcionarioAlterado.Telefone;
+            funcionarioOriginal.Senha    = funcionarioAlterado.Senha;
+            
+
+            if (ModelState.IsValid)
+            {
+                FuncionarioDAO.AlterarFuncionario(funcionarioOriginal);
+                return RedirectToAction("ClientesCadastrados", "Cliente");
+
+            }
+            else
+            {
+                return View(funcionarioOriginal);
+            }
+        }
+
         public ActionResult Login()
         {
             return View();
         }
+
         [HttpPost]
         public ActionResult Login([Bind(Include = "CPF, Senha")] Funcionario funcionario)
         {
